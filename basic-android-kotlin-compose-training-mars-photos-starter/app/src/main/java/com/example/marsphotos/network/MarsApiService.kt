48832beee8f0193ace48d5aaae03b8c830f34c4d
Assanalikhan <210103042@stu.sdu.edu.kz>
2023-12-13 19:54:49 +0600

    Third

--- basic-android-kotlin-compose-training-mars-photos-starter/app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
+++ basic-android-kotlin-compose-training-mars-photos-starter/app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
@@ -3,16 +3,22 @@ package com.example.marsphotos.network
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
-class MarsApiService {
-    private const val BASE_URL = "https://android-kotlin-fun-mars-server.appspot.com"
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 
-    private val retrofit = Retrofit.Builder()
-        .addConverterFactory(ScalarsConverterFactory.create())
-        .baseUrl(BASE_URL)
-        .build()
+private const val BASE_URL = "https://android-kotlin-fun-mars-server.appspot.com"
 
-    interface MarsApiService {
-        @GET("photos")
-        fun getPhotos(): String
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(ScalarsConverterFactory.create())
+    .baseUrl(BASE_URL)
+    .build()
+
+interface MarsApiService {
+    @GET("photos")
+    suspend fun getPhotos(): String
+}
+object MarsApi {
+    val retrofitService : MarsApiService by lazy {
+        retrofit.create(MarsApiService::class.java)
     }
 }
